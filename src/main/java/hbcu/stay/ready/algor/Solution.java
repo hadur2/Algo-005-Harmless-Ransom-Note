38e3404a22adcb017ad63ac468d98cdf45edcc3f
hadur2 <hadur@morgan.edu>
2019-08-06 10:07:49 -0400

    finish

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,31 @@
 package hbcu.stay.ready.algor;
 
+import java.util.Hashtable;
+
 public class Solution {
 
     public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+
+        String Message = " ";
+        String MagazineText = " ";
+
+        Hashtable<String, Integer> magazineHash = new Hashtable<String, Integer>();
+        boolean isValid = true;
+
+        for(String word: magazineText.split(" ")) {
+            Integer count = magazineHash.get(word);
+            if(count != null) {
+                magazineHash.put(word, count +1) ;
+            } else {
+                magazineHash.put(word, 1) ;
+            }
+        }
+
+        for(String word: message.split(" ")) {
+            Integer count = magazineHash.get(word);
+
+            if(count == null || count == 0) {
+
+
+        return isValid;
     }
-}
